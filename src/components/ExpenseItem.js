81172e7020f65c406b830f71933e725e909dcc31
mukulyadav5542 <mukulyadavjal@gmail.com>
2023-07-31 13:44:52 +0530

    Expense Item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item!</h2>
+      <div>
+        <h2>Food Rs 10</h2>
+        <h2>Petrol Rs 100</h2>
+        <h2>Movies Rs 200</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
