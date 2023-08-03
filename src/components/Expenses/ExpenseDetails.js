2d5196c838ea483340fb9e92f13e1752b6a85b40
mukulyadav5542 <mukulyadavjal@gmail.com>
2023-08-03 12:16:39 +0530

    Change Title and Delete Expense via DOM Manipul.

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -2,11 +2,21 @@ import React from "react";
 import Card from '../UI/Card'
 
 const ExpenseDetails = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!!!!');
+  }
+
+  const deleteHandler = () => {
+    console.log('Delete-Expense');
+  }
+
   return (
     <Card className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">Rs{props.amount}</div>
       <div>{props.locationOfExpenditure}</div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Expense</button>
       {/* <h2>Food Rs 10</h2>
         <h2>Petrol Rs 100</h2>
         <h2>Movies Rs 200</h2> */}
