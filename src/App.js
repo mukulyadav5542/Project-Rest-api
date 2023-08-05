1cdb6e753fe0d6298f46974d195d39e1c3e444a6
mukulyadav5542 <mukulyadavjal@gmail.com>
2023-08-05 17:25:09 +0530

    Rendering expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -5,60 +5,94 @@ import React, { useState } from "react";
 
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import NewExpense from "./components/NewExpense/NewExpense";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    locationOfExpenditure: "Big Mart",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    locationOfExpenditure: "TV Showroom",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure: "Car Showroom",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    locationOfExpenditure: "Carpenter Shop",
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Big Mart",
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), locationOfExpenditure: "TV Showroom", },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      locationOfExpenditure: "Car Showroom",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      locationOfExpenditure: "Carpenter Shop",
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   // return React.createElement(
-  //   'div', 
-  //   {}, 
+  //   'div',
+  //   {},
   //   React.createElement('h2', {}, "Let's get started!"),
   //   React.createElement(ExpenseItem, { items: expenses })
   // );
 
-  const [expenseData, setExpensesData] = useState(expenses)
+  //const [expenseData, setExpensesData] = useState(expenses);
 
-  const addExpenseHandler = expense => {
+  const addExpenseHandler = (expense) => {
     // console.log('In App.js');
-    setExpensesData([...expenseData, expense])
-    console.log(expense);
+    //setExpensesData([...expenseData, expense]);
+    //console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
       <div>
-      {expenseData.map((item) => (
-        <ExpenseItem
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-          locationOfExpenditure={item.locationOfExpenditure}
-        />
-      ))}
+        <div>
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+        </div>
+        {/* {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            locationOfExpenditure={expense.locationOfExpenditure}
+          />
+        ))} */}
+        {expenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+            locationOfExpenditure={item.locationOfExpenditure}
+          />
+        ))}
       </div>
       {/* <ExpenseItem
         title={expenses[1].title}
@@ -80,6 +114,6 @@ const App = () => {
       ></ExpenseItem> */}
     </div>
   );
-}
+};
 
 export default App;
